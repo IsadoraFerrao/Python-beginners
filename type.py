29b71cc92fb22952dca0e33e075862b9b7a3b4a2
DESKTOP-2N7HPJ1\isado <isadora-gf@hotmail.com>
2020-11-01 20:16:33 -0300

    types

---
+++ type.py
@@ -0,0 +1,16 @@
+
+lista_A=['a', 'c', 'b', 'd']
+num = [0,1,2,3,9,3]
+#print(type(lista_A[0])) #pegar tipo
+
+#curso = lista_A.pop()
+#lista_A.sort() #coloca em ordem alfabetica
+#sorted coloca numeros na ordem
+list = num.count(3) #contar o numero q tem na lista, se for string coloca aspas
+
+#len é uma função para contar a quantidade na lista
+#print(len(lista_A))
+
+#lista_A_str = ' -- '.join(lista_A)
+#for posicao, curso in enumerate(num, start=1): #mostrar posicoes da lista
+#    print(f"{posicao}){curso}")
