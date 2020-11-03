a6b7b4886f632a7a6f8616b2d4e15e2e346cfe18
DESKTOP-2N7HPJ1\isado <isadora-gf@hotmail.com>
2020-11-03 20:14:48 -0300

    random

---
+++ for-random.py
@@ -0,0 +1,13 @@
+# for algo in grupo
+    #funcao algo
+    
+#for num in '987654':
+#    print(num)
+
+#for num in 'isadora':
+#    print(num)
+
+import random 
+for sorteio in range(2): #quantos sorteios desejo
+    num_sorted=random.randint(10,50)
+    print(num_sorted)
