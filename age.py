eb135e6bfce1e5ed08c0c659c94d718d4ba0dfab
DESKTOP-2N7HPJ1\isado <isadora-gf@hotmail.com>
2020-11-05 20:06:51 -0300

    age

---
+++ age.py
@@ -0,0 +1,10 @@
+print("Olá, eu sou a Lu do Magalu. ")
+print("Qual o seu nome?")
+nome = input()
+
+print(f"Que prazer em conhecê-lo, {nome}")
+print("Qual a sua idade?")
+idade = input()
+
+qntd_idade = 107-int(idade)
+print(f"Eu sou {qntd_idade} mais velha que você, {nome}")
