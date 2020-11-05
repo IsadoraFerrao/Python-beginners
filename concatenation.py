2968ceef3754da60ed8ea08664f797d17b175559
DESKTOP-2N7HPJ1\isado <isadora-gf@hotmail.com>
2020-11-05 20:08:34 -0300

    concatenation

---
+++ concatenation.py
@@ -0,0 +1,10 @@
+# "%s, %s. Bem-Vinda!" %(saudacao, nome, aula)
+# “{}, {}. Bem-Vinda!”.format(saudacao, nome, aula)
+# F-string f"{saud}, {nome}. Bem-Vinda! Voce tem {idade} anos"
+
+a= 'ola'
+b= 'maria'
+c= 1
+print("%s %s" %(a,b))
+print ("Bom dia " + "%s" %(b))
+print(f"Ola {b}")
