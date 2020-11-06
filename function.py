9a9b81ada341c1f5d8ee01b7dea5b0dcc03d5566
DESKTOP-2N7HPJ1\isado <isadora-gf@hotmail.com>
2020-11-06 20:46:57 -0300

    function

---
+++ function.py
@@ -0,0 +1,9 @@
+def fala_ola(sentimento, nome = 'você'):
+    return f"Eu {sentimento} {nome}"
+
+#print(fala_ola('adoro', 'Maria'))
+
+def adicao(num1, num2):
+    return num1 + num2
+
+print(adicao(3,2))
