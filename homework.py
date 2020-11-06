36e6f0044792d8741e643b873604e169c1155545
DESKTOP-2N7HPJ1\isado <isadora-gf@hotmail.com>
2020-11-06 19:40:54 -0300

    homework update

--- homework.py
+++ homework.py
@@ -5,29 +5,55 @@ import random
 
 print("Olá, qual o seu nome?")
 nome = input()
-print(f"\nOlá {nome}, estou pensando em um número de 0 a 20.")
+print(f"\nOlá {nome}, estou pensando em um número de 0 a 80.")
 print("Você consegue adivinhar?")
 print("(1) Sim (2) Não")
 entrada = int(input())
 
-num_sorteado = random.randint(1, 20)
+num_sorteado = random.randint(1, 81)
+cont = 0
+resposta= "JOGAR"
 
 if(entrada == 1):
-    for cont in range(0,6):
+    while(resposta== "JOGAR"):
         print("Qual número estou pensando?")
         numero = int(input())
+        
         if(numero > num_sorteado):
-            print("O número é menor, tente novamente")
-            cont = cont+1
+            if((num_sorteado - numero) <= 10 ):
+                print("O número é menor, tente novamente! Você está quase lá")
+                cont = cont+1
+            else:
+                print("Você ainda está longe, se esforce mais")
+                cont=cont+1
         elif(numero < num_sorteado):
-            print("O número é maior, tente novamente")
-            cont=cont+1
+            if((num_sorteado - numero) <= 10):
+                print("O número é maior, tente novamente! Você está quase lá")
+                cont=cont+1
+            else:
+                print("Você ainda está longe, se esforce mais")
+                cont=cont+1
+        elif(numero == num_sorteado):
+            if(cont <= 5):
+                print(f"Parabéns {nome}, você acertou o número na {cont+1}° tentativa")
+                cont = 0
+                print("Quer tentar novamente?")
+                print("(1) Sim (2) Não")
+                entrada = int(input()) 
+                if(entrada == 1):
+                    resposta == "JOGAR"
+                else:
+                    exit()
+            else:
+                print(f"Aleluia! Você acertou na {cont+1}")
+                cont = 0
+                print("Quer tentar novamente?")
+                print("(1) Sim (2) Não")
+                entrada = int(input()) 
+                if(entrada == 1):
+                    resposta == "JOGAR"
+                else:
+                    exit()
         else:
-            print(f"Parabéns {nome}, você acertou o número na {cont+1}° tentativa")
+            print("Na próxima quem sabe, bye bye")
             exit()
-    print("Suas chances terminaram")
-    print(f"O número correto seria {num_sorteado}")
-    exit()
-else:
-    print("Na próxima quem sabe, bye bye")
-    exit()
