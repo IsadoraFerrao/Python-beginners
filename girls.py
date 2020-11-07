00915a883b48f43c1d4605b589ffacca81e6aaf9
DESKTOP-2N7HPJ1\isado <isadora-gf@hotmail.com>
2020-11-07 11:10:43 -0300

    add lists-compreehension

---
+++ girls.py
@@ -0,0 +1,9 @@
+def grandes_mulheres(nome1, nome2):
+    if(nome1 or nome2 == 'Ada Lovelace'):
+        print("Primeira programadora da história")
+    elif(nome1 == 'Grace Hopper'):
+        print("A maior geek da história")
+    else:
+        print("Nome desconhecido")
+
+grandes_mulheres("Ada Lovelace", "Grace Hopper")
