bc45d0dbc6944a76d5bf4066901efbd3a43c4a22
DESKTOP-2N7HPJ1\isado <isadora-gf@hotmail.com>
2020-11-06 21:35:08 -0300

    add lists

--- list_function.py
+++ list_function.py
@@ -1,4 +1,7 @@
-cursos_arts = ['arte', 'canto', 'pintura']
+cursos_arts = {'arte', 'canto', 'pintura'}
 
+#curso1 = cursos_arts.pop()
+cursos_arts.add('python')
 print(cursos_arts)
 print('arte' in cursos_arts) #verificar se contem arte na lista de cursos
+
