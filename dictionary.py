78a5f5e382ed177d1a2b29d08c026b52cc59bb08
DESKTOP-2N7HPJ1\isado <isadora-gf@hotmail.com>
2020-11-07 17:15:24 -0300

    dictionary

--- dictionary.py
+++ dictionary.py
@@ -2,8 +2,11 @@ funcionario = {'nome': 'Thor', 'idade':35, 'salario':7350.34,
 'funcao':['ser lindo']}
 
 funcionario['estavo_civil'] = 'solteiro' #add
-del funcionario['idade'] #deletar
-print(funcionario.get('salario')) #pegar algo
+#del funcionario['idade'] #deletar
+#print(funcionario.get('salario')) #pegar algo
 #print(funcionario)
-
+#pop deletar o último item da lista
 #para atualizar basta usar atribuicao
+
+for coisa in funcionario.items(): #mostra a lista
+    print(coisa)
