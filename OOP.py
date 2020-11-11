0f630f1c2f02bba026e3214bafa8743464718a11
DESKTOP-2N7HPJ1\isado <isadora-gf@hotmail.com>
2020-11-11 19:21:48 -0300

    dictionary

--- OOP.py
+++ OOP.py
@@ -5,4 +5,23 @@ class Atores:
         self.sexo = sexo
         self.nacionalidade = nacionalidade
 
-    
+    def get_origin(self):
+        origens = {'american': 'from the U.S', 'brazilian': 'from Brazil', 'mexican':'from Maxico'}
+        if self.nacionalidade.lower() in origens.keys():
+            origin = origins[self.nacionalidade.lower()]
+            return origin
+        else:
+            return f"I'm {self.nacionalidade}"
+
+    def actor_intro(self):
+        return f"Hi, my name is {self.name} and I'm a {self.sexo}"
+
+class Big_Bang(Atores):
+    pass
+
+act1 = Big_Bang("Rodrigo", 45, "masculino", "Brasileiro")
+act2 = Atores("Rodrigo Santoro", 45, "masculino", "Brasileiro")
+
+print(act1.nome)
+#print(act1.get_origin())
+#print(act1.nacionalidade)
