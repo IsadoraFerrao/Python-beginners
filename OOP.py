3d30130c9f78932a1d1f3b4c48cef5ed33df6266
DESKTOP-2N7HPJ1\isado <isadora-gf@hotmail.com>
2020-11-11 18:57:08 -0300

    dictionary

---
+++ OOP.py
@@ -0,0 +1,8 @@
+class Atores:
+    def __init__(self, nome, idade, sexo, nacionalidade):
+        self.nome = nome
+        self.idade = idade
+        self.sexo = sexo
+        self.nacionalidade = nacionalidade
+
+    
