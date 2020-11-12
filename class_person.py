2f855db5e7e5209804d2c23675eca354ed842c93
DESKTOP-2N7HPJ1\isado <isadora-gf@hotmail.com>
2020-11-12 19:29:31 -0300

    class person

--- class_person.py
+++ class_person.py
@@ -1,8 +1,16 @@
 class Pessoa:
-    def __init__(self, nome, sexo, id):
+    def __init__(self, nome, idade, sexo, id):
         self.nome = nome
+        self.idade = idade
         self.sexo = sexo
         self.id = id
+    def maioridade(self):
+        idade = self.idade
+        if(idade >= 18):
+            return "maioridade"
+        else:
+            return "menor de idade"
 
-pessoa = Pessoa("Deb", "feminino", 25585)
+pessoa = Pessoa("Deb", 10, "feminino", 25585)
 print(pessoa.nome)
+print(pessoa.maioridade())
