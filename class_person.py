302d13ffd31d1a34529e45464530b0b7b83fc539
DESKTOP-2N7HPJ1\isado <isadora-gf@hotmail.com>
2020-11-12 19:02:02 -0300

    class person

--- class_person.py
+++ class_person.py
@@ -1,6 +1,8 @@
 class Pessoa:
     def __init__(self, nome, sexo, id):
-    self.nome = nome
-    self.idade = idade
-    self.sexo = sexo
-    self.id = id
+        self.nome = nome
+        self.sexo = sexo
+        self.id = id
+
+pessoa = Pessoa("Deb", "feminino", 25585)
+print(pessoa.nome)
